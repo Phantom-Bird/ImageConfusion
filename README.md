7d1d12492adb7d99a0f0eea05aff6cdcbe5ed98e
Phantom-Bird <96868057+Phantom-Bird@users.noreply.github.com>
2025-07-21 12:39:10 +0800

    edit readme

--- README.md
+++ README.md
@@ -11,6 +11,8 @@ python __main__.py
 
 ## 示例
 
+示例均使用默认设置：
+
 ![](example/image.png)
 
 ![](example/confused.png)
@@ -19,4 +21,6 @@ python __main__.py
 
 ![](example/compressed.jpg) → ![](example/final.jpg)
 
-我们发现只是因为强行打破像素连续性，导致压缩后出现了噪点。
+我们发现只是因为强行打破像素连续性，导致压缩后出现了噪点。这可以通过加粗网格，增加缓冲避免。
+
+图上的白线可以通过调整阈值消除。
--- __generate_readme_pictures.py
+++ __generate_readme_pictures.py
@@ -1,6 +1,7 @@
 from confusion import confuse, deconfuse
 from PIL import Image
 from config import *
+import config as cfg
 
 img = Image.open('example/image.png')
 con_img = confuse(img, (BLOCK_SIZE, BLOCK_SIZE), GRID_WIDTH, RULER_SIZE, DEFAULT_SEED)
@@ -8,4 +9,3 @@ con_img.save('example/confused.png')
 con_img.resize((img.width // 2, img.height // 2)).save('example/compressed.jpg', optimize=True, quality=50)
 con_compressed = Image.open('example/compressed.jpg')
 deconfuse(con_compressed, DEFAULT_SEED).save('example/final.jpg')
-img.resize((img.width // 2, img.height // 2)).save('example/comparison.jpg', optimize=True, quality=50)
