e0c70c947591ddca64670da3207de44faf9b6936
Phantom-Bird <96868057+Phantom-Bird@users.noreply.github.com>
2025-07-21 08:50:30 +0800

    fix bugs

--- __generate_readme_pictures.py
+++ __generate_readme_pictures.py
@@ -7,4 +7,4 @@ con_img = confuse(img, (BLOCK_SIZE, BLOCK_SIZE), GRID_WIDTH, RULER_SIZE, DEFAULT
 con_img.save('example/confused.png')
 con_img.resize((img.width // 3, img.height // 3)).save('example/compressed.jpg', optimize=True, quality=30)
 con_compressed = Image.open('example/compressed.jpg')
-deconfuse(con_img, DEFAULT_SEED).save('example/final.jpg')
+deconfuse(con_compressed, DEFAULT_SEED).save('example/final.jpg')
